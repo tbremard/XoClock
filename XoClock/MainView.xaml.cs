fac68e939299d4a9aa808b7a4be95e92eab48ca3
tbremard <t.bremard@gmail.com>
2020-05-28 20:36:06 +0200

    clock can move with left click

--- XoClock/MainView.xaml.cs
+++ XoClock/MainView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace XoClock
 {
@@ -7,6 +8,9 @@ namespace XoClock
     /// </summary>
     public partial class MainView : Window
     {
+        bool _isMoving = false;
+        Point _lastPosition;
+
         public MainView()
         {
             InitializeComponent();
@@ -19,6 +23,27 @@ namespace XoClock
             Left = screenWidth-Width;
             var clock = new TimerModel();
             DataContext = new MainViewModel(clock);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMoving = true;
+            _lastPosition = e.GetPosition(this); 
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isMoving = false;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isMoving)
+            {
+                Point currentPosition = e.GetPosition(this);
+                Left += currentPosition.X - _lastPosition.X;
+                Top += currentPosition.Y - _lastPosition.Y;
+            }
         }
     }
 }
