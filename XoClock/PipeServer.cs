087157832cdbd5443cf2a05132774edd6772bdb7
tbremard <t.bremard@gmail.com>
2020-12-13 15:29:15 +0100

    slight refactor

--- XoClock/PipeServer.cs
+++ XoClock/PipeServer.cs
@@ -18,57 +18,58 @@ namespace XoClock
 
         public void OpenPort()
         {
-            using (NamedPipeServerStream pipeServer =
-                        new NamedPipeServerStream(PipeConst.XOCLOCK_PIPE_NAME, PipeDirection.InOut))
+            var pipeServer = new NamedPipeServerStream(PipeConst.XOCLOCK_PIPE_NAME, PipeDirection.InOut);
+            _log.Debug("Waiting for client connection...");
+            pipeServer.WaitForConnection();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            _log.Debug("Client connected on thread[{0}].", threadId);
+            try
             {
-                _log.Debug("Waiting for client connection...");
-                pipeServer.WaitForConnection();
-                int threadId = Thread.CurrentThread.ManagedThreadId;
-                _log.Debug("Client connected on thread[{0}].", threadId);
-                try
-                {
-                    // Read the request from the client. Once the client has
-                    // written to the pipe its security token will be available.
-                    StreamString ss = new StreamString(pipeServer);
-                    // Verify our identity to the connected client using a
-                    // string that the client anticipates.
-                    ss.WriteString(PipeConst.XOCLOCK_SERVER_ID);
-                    string command = ss.ReadString();
-                    // Display the name of the user we are impersonating.
-                    _log.Debug("received command: '{0}' on thread[{1}] as user: {2}.",
-                                        command, threadId, pipeServer.GetImpersonationUserName());
+                // Read the request from the client. Once the client has
+                // written to the pipe its security token will be available.
+                StreamString ss = new StreamString(pipeServer);
+                // Verify our identity to the connected client using a
+                // string that the client anticipates.
+                ss.WriteString(PipeConst.XOCLOCK_SERVER_ID);
+                string command = ss.ReadString();
+                // Display the name of the user we are impersonating.
+                _log.Debug("received command: '{0}' on thread[{1}] as user: {2}.",
+                                    command, threadId, pipeServer.GetImpersonationUserName());
+                DispatchToModel(command);
+            }
+            catch (IOException e)
+            {
+                _log.Debug("ERROR: {0}", e.Message);
+            }
+            pipeServer.Close();
+            pipeServer.Dispose();
+        }
 
-                    if(command == XoClockCommand.START_CHRONO.ToString())
-                    {
-                        _viewModel.StartChrono();
-                    }
-                    if (command == XoClockCommand.STOP_CHRONO.ToString())
-                    {
-                        _viewModel.StopChrono();
-                    }
-                    if (command == XoClockCommand.RESET_CHRONO.ToString())
-                    {
-                        _viewModel.ResetChrono();
-                    }
-                    if (command == XoClockCommand.MODE_CHRONO.ToString())
-                    {
-                        _viewModel.SetMode(ClockMode.Chronometer);
-                    }
-                    if (command == XoClockCommand.MODE_CLOCK.ToString())
-                    {
-                        _viewModel.SetMode(ClockMode.Clock);
-                    }
-                    if (command == XoClockCommand.KILL.ToString())
-                    {
-                        Application.Current.Dispatcher.InvokeShutdown();
-                    }
-                }
-                catch (IOException e)
-                {
-                    _log.Debug("ERROR: {0}", e.Message);
-                }
-
-                pipeServer.Close();
+        private void DispatchToModel(string command)
+        {
+            if (command == XoClockCommand.START_CHRONO.ToString())
+            {
+                _viewModel.StartChrono();
+            }
+            if (command == XoClockCommand.STOP_CHRONO.ToString())
+            {
+                _viewModel.StopChrono();
+            }
+            if (command == XoClockCommand.RESET_CHRONO.ToString())
+            {
+                _viewModel.ResetChrono();
+            }
+            if (command == XoClockCommand.MODE_CHRONO.ToString())
+            {
+                _viewModel.SetMode(ClockMode.Chronometer);
+            }
+            if (command == XoClockCommand.MODE_CLOCK.ToString())
+            {
+                _viewModel.SetMode(ClockMode.Clock);
+            }
+            if (command == XoClockCommand.KILL.ToString())
+            {
+                Application.Current.Dispatcher.InvokeShutdown();
             }
         }
     }
