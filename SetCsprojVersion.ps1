654c2bbeab76ccb08939d25ee17da163eddc708e
Thierry Bremard <t.bremard@gmail.com>
2022-03-11 20:13:39 +0100

    update powershell to set version

--- SetCsprojVersion.ps1
+++ SetCsprojVersion.ps1
@@ -1,11 +1,12 @@
 $csprojPath = $args[0] # path to the file i.e. 'C:\Users\ben\Code\csproj powershell\MySmallLibrary.csproj'
 $newVersion = $args[1] # the build version, from VSTS build i.e. "1.1.20170323.1"
 
-Write-Host "Starting process of generating new version number for the csproj"
+Write-Host "Patch csproj with version number"
 $x=Get-Location
-Write-Host "CurrentDirectory: "$x.Path
 $csprojPath = $x.Path+"\"+$csprojPath
-Write-Host ("filePath: " + $csprojPath)
+Write-Host("CurrentDirectory: "+$x.Path)
+Write-Host("FilePath        : " + $csprojPath)
+Write-Host("Version         : " + $newVersion)
 $splitNumber = $newVersion.Split(".")
 if( $splitNumber.Count -eq 4 )
 {
@@ -15,7 +16,7 @@ if( $splitNumber.Count -eq 4 )
 	$xml.Project.PropertyGroup[0].AssemblyVersion = $newVersion
 	$xml.Project.PropertyGroup[0].Version = $newVersion
 	$xml.Save($csprojPath)
-	Write-Host "Updated csproj "$csprojPath" and set to FileVersion "$newVersion
+	Write-Host "Updated "$csprojPath" with version: "$newVersion
 }
 else
 {
